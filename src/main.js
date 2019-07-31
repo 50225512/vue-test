02afebea40105a7a668ebd80a2f7349537c52beb
50225512 <h249139571@126.com>
2019-07-31 21:36:40 +0800

    搭建页面框架

--- src/main.js
+++ src/main.js
@@ -2,10 +2,24 @@
 import Vue from 'Vue'
 import app from './App.vue'
 import './lib/mui/css/mui.min.css'
-import { Header } from 'mint-ui'
+import './lib/mui/css/icons-extra.css'
+import { Header,Swipe, SwipeItem } from 'mint-ui'
 Vue.component(Header.name, Header)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
+
+//路由
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+import router from './router.js'
+
+//导入 vue-resource
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+
 
 var vm = new Vue({
     el:'#app',
-    render: c=> c(app)
+    render: c=> c(app),
+    router
 })
